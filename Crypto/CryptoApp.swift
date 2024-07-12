46da817c49bfeec06ee5272424125576085ffe53
saiteja7171 <saiteja7171@gmail.com>
2024-07-12 09:51:29 -0500

    Initial Commit

---
+++ Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Sai Teja Atluri on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
